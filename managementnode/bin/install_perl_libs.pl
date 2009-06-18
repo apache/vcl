ff93ec5a2cfc89e30dfb867b87b8caf012e15979
Andrew Kurth <arkurth@apache.org>
2009-06-18 18:22:35 +0000

    VCL-164 Forgot to set svn properties on bin/install_perl_libs.pl when I added. Set properties to eol-style=native, keywords="Date Revision Author HeadURL Id", executable=*.

--- managementnode/bin/install_perl_libs.pl
+++ managementnode/bin/install_perl_libs.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl -w
 ##############################################################################
-# $Id: $
+# $Id$
 ##############################################################################
 # Licensed to the Apache Software Foundation (ASF) under one or more
 # contributor license agreements.  See the NOTICE file distributed with
