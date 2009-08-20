543f83ebe1230c2dfeaf6ab8461c221394e27009
Andrew Kurth <arkurth@apache.org>
2009-08-20 19:51:07 +0000

    VCL-165 Added check in vmware.pm where it is listing the contents of the VM host's data store. If the command fails, the array returned was undefined and this error occurred:
    Can't use an undefined value as an ARRAY reference at
     /usr/local/vcl/bin/../lib/VCL/Module/Provisioning/vmware.pm line 263.
    
    This caused the process to die immediately and the process was left in the pending state.

--- managementnode/lib/VCL/Module/Provisioning/vmware.pm
+++ managementnode/lib/VCL/Module/Provisioning/vmware.pm
@@ -260,7 +260,14 @@ sub load {
 			notify($ERRORS{'OK'}, 0, "listing datestore $datastorepath ");
 			undef @sshcmd;
 			@sshcmd = run_ssh_command($hostnode, $identity, "ls -1 $datastorepath", "root");
-			notify($ERRORS{'OK'}, 0, "@{ $sshcmd[1] }");
+			if (!defined(@{$sshcmd[1]})) {
+				notify($ERRORS{'CRITICAL'}, 0, "failed to list data store contents $datastorepath on vm host");
+				insertloadlog($reservation_id, $vmclient_computerid, "failed", "failed to list data store contents $datastorepath on vm host");
+				close(TMPLOCK);
+				unlink($tmplockfile);
+				return 0;
+			}
+			notify($ERRORS{'OK'}, 0, "data store contents $datastorepath on vm host:\n@{ $sshcmd[1] }");
 			foreach my $l (@{$sshcmd[1]}) {
 				if ($l =~ /denied|No such/) {
 					notify($ERRORS{'CRITICAL'}, 0, "node $hostnode output @{ $sshcmd[1] }");
