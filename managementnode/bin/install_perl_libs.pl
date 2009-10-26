85860171d285c57debc3192fa4a78f0a92acdba0
Andrew Kurth <arkurth@apache.org>
2009-10-26 19:35:25 +0000

    Added disclaimer to install_perl_libs.pl notifying user of the licenses the Perl modules which the script downloads and installs are distributed under. The user has to enter YES in order for the script to run.
    Added to Management Node Perl Modules section in README:
    -blurb about the types of licenses the dependency modules are distributed under
    -the license type and URL for each Perl module dependency
    
    Added URI and Scalar-List-Utils to list of Perl module dependencies. These needed to be added after uninstalling and reinstalling Perl while testing install_perl_libs.pl. Updated the versions of some of the Perl modules. Added perl-DBD-MySQL to Management Node Backend section in README. I noticed I had to install this separately when testing the changes to install_perl_libs.pl.

--- managementnode/bin/install_perl_libs.pl
+++ managementnode/bin/install_perl_libs.pl
@@ -40,40 +40,63 @@ use strict;
 use warnings;
 use diagnostics;
 
-my $download_directory= '/tmp/perl-modules';
-if (-e $download_directory) {
-	#unlink $download_directory;
+my $disclaimer .= <<"EOF";
+==============================================================================
+*** NOTICE ***
+
+This script will download and install Perl modules distributed under
+the following licenses:
+- The "Artistic License"
+- GNU General Public License (GPL)
+- GNU Library or "Lesser" General Public License (LGPL)
+
+See the README file for more information.
+==============================================================================
+EOF
+
+print $disclaimer;
+while (1) {
+	print 'Type YES to proceed, type NO to abort: ';
+	my $input = <>;
+	if ($input =~ /^\s*YES\s*$/i) {
+		last;
+	}
+	elsif ($input =~ /^\s*NO\s*$/i) {
+		exit;
+	}
+	else {
+		next;
+	}
 }
+
+print "==============================================================================\n";
+
+my $download_directory= '/tmp/perl-modules';
 mkdir $download_directory;
 
 my @module_urls = (
 	'http://search.cpan.org/CPAN/authors/id/M/MA/MARKOV/MailTools-2.04.tar.gz',
-	
 	'http://search.cpan.org/CPAN/authors/id/T/TM/TMTM/Class-Data-Inheritable-0.08.tar.gz',
-	'http://search.cpan.org/CPAN/authors/id/D/DR/DROLSKY/Devel-StackTrace-1.20.tar.gz',
-	'http://search.cpan.org/CPAN/authors/id/D/DR/DROLSKY/Exception-Class-1.26.tar.gz',
-	'http://search.cpan.org/CPAN/authors/id/J/JD/JDHEDDEN/Object-InsideOut-3.52.tar.gz',
-	
-	'http://search.cpan.org/CPAN/authors/id/K/KW/KWILLIAMS/Module-Build-0.30.tar.gz',
+	'http://search.cpan.org/CPAN/authors/id/D/DR/DROLSKY/Devel-StackTrace-1.22.tar.gz',
+	'http://search.cpan.org/CPAN/authors/id/D/DR/DROLSKY/Exception-Class-1.29.tar.gz',
+	'http://search.cpan.org/CPAN/authors/id/J/JD/JDHEDDEN/Object-InsideOut-3.56.tar.gz',
+	'http://search.cpan.org/CPAN/authors/id/D/DA/DAGOLDEN/Module-Build-0.35.tar.gz',
 	'http://search.cpan.org/CPAN/authors/id/H/HA/HACKER/Net-XMPP-1.02.tar.gz',
 	'http://search.cpan.org/CPAN/authors/id/A/AG/AGROLMS/GSSAPI-0.26.tar.gz',
 	'http://search.cpan.org/CPAN/authors/id/G/GA/GAAS/Digest-SHA1-2.12.tar.gz',
-	'http://search.cpan.org/CPAN/authors/id/G/GA/GAAS/Digest-HMAC-1.01.tar.gz',
-	'http://search.cpan.org/CPAN/authors/id/G/GB/GBARR/Authen-SASL-2.12.tar.gz',
+	'http://search.cpan.org/CPAN/authors/id/G/GB/GBARR/Authen-SASL-2.13.tar.gz',
 	'http://search.cpan.org/CPAN/authors/id/R/RE/REATMON/XML-Stream-1.22.tar.gz',
 	'http://search.cpan.org/CPAN/authors/id/R/RE/REATMON/Net-Jabber-2.0.tar.gz',
-	
-	'http://search.cpan.org/CPAN/authors/id/I/IN/INGY/YAML-0.68.tar.gz',
-	
-	'http://search.cpan.org/CPAN/authors/id/R/RJ/RJRAY/RPC-XML-0.64.tar.gz',
+	'http://search.cpan.org/CPAN/authors/id/A/AD/ADAMK/YAML-0.70.tar.gz',
+	'http://search.cpan.org/CPAN/authors/id/R/RJ/RJRAY/RPC-XML-0.69.tar.gz',
 	'http://www.cpan.org/modules/by-module/XML/XML-Parser-2.36.tar.gz',
 	'http://www.cpan.org/modules/by-module/Crypt/Crypt-SSLeay-0.57.tar.gz',
-	'http://search.cpan.org/CPAN/authors/id/P/PM/PMQS/Compress-Raw-Zlib-2.020.tar.gz',
-	'http://search.cpan.org/CPAN/authors/id/P/PM/PMQS/IO-Compress-2.020.tar.gz',
-	
+	'http://search.cpan.org/CPAN/authors/id/P/PM/PMQS/Compress-Raw-Zlib-2.021.tar.gz',
+	'http://search.cpan.org/CPAN/authors/id/P/PM/PMQS/IO-Compress-2.022.tar.gz',
 	'http://search.cpan.org/CPAN/authors/id/T/TI/TIMB/DBI-1.609.tar.gz',
-	
-	'http://search.cpan.org/CPAN/authors/id/G/GA/GAAS/libwww-perl-5.827.tar.gz',
+	'http://search.cpan.org/CPAN/authors/id/G/GA/GAAS/libwww-perl-5.833.tar.gz',
+	'http://search.cpan.org/CPAN/authors/id/G/GA/GAAS/URI-1.40.tar.gz',
+	'http://search.cpan.org/CPAN/authors/id/G/GB/GBARR/Scalar-List-Utils-1.21.tar.gz'
 );
 
 # Loop through each URL
@@ -89,19 +112,20 @@ for my $url (@module_urls) {
 	my ($module_package) = $module_name =~ /([^\/]+)-[\d\.]+$/;
 	$module_package =~ s/-/::/g;
 	
-	# Fix module package names
+	# Fix module package names and set arguments as necessary
 	if ($module_name =~ /libwww-perl/) {
 		$module_package = "Bundle::LWP";
 	}
 	elsif ($module_name =~ /MailTools/) {
 		$module_package = "Mail::Mailer";
 	}
-	elsif ($module_name =~ /TimeDate/) {
-		$module_package = "Date::Parse";
-	}
 	elsif ($module_name =~ /IO-Compress/) {
 		$module_package = "Compress::Zlib";
 	}
+	elsif ($module_name =~ /Scalar-List-Utils/) {
+		$module_package = "Scalar::Util";
+	}
+	
 	print "Module package: $module_package\n";
 	
 	if (!module_installed($module_package)) {
@@ -130,8 +154,9 @@ sub module_installed {
 	
 	my $output = `perl -M$module_package -e '' 2>&1`;
 	my $exit_status = $? >> 8;
+	#print "Checked if $module_package is installed, output:\n---\n$output\n---\n";
 	
-	if ($output !~ /Can't locate/i) {
+	if ($output !~ /Can't locate/) {
 		print "Module is already installed: $module_package\n";
 		return 1;
 	}
