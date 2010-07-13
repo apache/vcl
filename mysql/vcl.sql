01f6e2dc817ef3b9e5b4110862ba607278c74bb0
Frank Aaron Peeler <fapeeler@apache.org>
2010-07-13 20:00:43 +0000

    VCL-178
    default value should be 1

--- mysql/vcl.sql
+++ mysql/vcl.sql
@@ -643,7 +643,7 @@ CREATE TABLE IF NOT EXISTS `request` (
   `end` datetime NOT NULL default '0000-00-00 00:00:00',
   `daterequested` datetime NOT NULL default '0000-00-00 00:00:00',
   `datemodified` datetime default NULL,
-  `checkuser` tinyint(1) unsigned NOT NULL default '0',
+  `checkuser` tinyint(1) unsigned NOT NULL default '1',
   PRIMARY KEY  (`id`),
   KEY `userid` (`userid`),
   KEY `stateid` (`stateid`),
