6b96dd086ada532fd3591fab3e67b58a96045537
Josh Thompson <jfthomps@apache.org>
2012-04-26 18:10:51 +0000

    VCL-485 Multilingualization of Web UI
    adding es_MX as contributed by Juan Jose Zamanillo

---
+++ web/js/nls/es_MX/messages.js
@@ -0,0 +1,78 @@
+/*
+* Licensed to the Apache Software Foundation (ASF) under one or more
+* contributor license agreements.  See the NOTICE file distributed with
+* this work for additional information regarding copyright ownership.
+* The ASF licenses this file to You under the Apache License, Version 2.0
+* (the "License"); you may not use this file except in compliance with
+* the License.  You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+nlsmessages = {
+'Times cannot be suggested for cluster reservations': 'No se pueden sugerir horarios para la reservación de clusters',
+'Cancel': 'Cancelar',
+'Okay': 'Correcto',
+'Modify Reservation': 'Modificar Reservación',
+'Please select a valid environment.': 'Por favor seleccione un ambiente valido',
+'View Available Times': 'ver horarios disponibles',
+'days': 'días',
+'hours': 'horas',
+'hour': 'hora',
+'Loading...': 'Cargando',
+'The end time must be later than the start time.': 'La hora final debe de ser más tarde que la hora inicial',
+'The reservation you selected<br>to edit has expired.<br><br>': 'La reservación que usted selecciono<br>para editar ha expirado.<br><br>',
+'The reservation you selected<br>to reboot has expired.<br><br>': 'La reservación que usted selecciono<br> que reiniciara ha expirado.<br><br>',
+'The reservation you selected<br>to reinstall has expired.<br><br>': 'La reservación que usted selecciono<br> para reinstalar ha expirado.<br><br>',
+'An invalid version was submitted.': 'Una versión inválida ha sido enviada.',
+' Ready to connect in ': 'Listo para conectarse en ',
+' second': 'segundo',
+' seconds': 'segundos',
+'match': 'concuerda',
+'no match': 'no concuerda',
+'Remove': 'Borrar',
+'Skip': 'Saltar',
+'Use': 'Use',
+'Name:': 'Nombre:',
+'Owner:': 'Dueño',
+'specified in comments': 'especificado en comentarios',
+'Monthly': 'Mensualmente',
+'First Date:': 'Primer fecha:',
+'Last Date:': 'Ultima fecha:',
+'Repeat on:': 'Repetir el:',
+' of each month': 'de cada mes:',
+'During these times:': 'En estos horarios:',
+'List of Dates/Times': 'Lista de fechas/horarios',
+'Weekly': 'Semanalmente:',
+'Repeating on these days:': 'Repetir en estos días:',
+'User Submitted Comments;': 'Comentarios enviados por el usuario;',
+'Total online: ': 'Total en línea',
+'These times overlap with an existing time slot': 'Este horario se empalma con una reservación existente',
+'The date must be today or later': 'La fecha debe de ser hoy o posterior',
+'The date and times overlap with an existing time slot': 'La fecha y hora se traslapa con una reservación existente',
+'Please fix invalid values before submitting.': 'Por favor corrija los valores inválidos antes de enviar',
+'Please fill in First Date of Usage': 'Por favor indique la primer fecha de uso',
+'Please fill in Last Date of Usage': 'Por favor indique la última fecha de uso',
+'The First Date of Usage must be today or later': 'La fecha de inicio debe de ser hoy o posterior',
+'The Last Date of Usage must be the same or later than the First Date of Usage': 'La última fecha debe de ser igual o posterior a la fecha inicial',
+'The Last Date of Usage must be today or later': 'La última fecha debe de ser hoy o posterior',
+'At least one day must be checked when using "Repeating Weekly"': 'Por lo menos debe de seleccionar un día cuando usa "Repetir semanalmente"',
+'At least one start/end combination must be entered when using "Repeating Weekly"': 'Por lo menos debe de seleccionar una combinación de inicio/fin cuando usa "Repetir semanalmente"',
+'When submitting "(group not listed)" as the user group, information must be included in the comments about what group needs to be created.': 'Cuando seleccione "(grupo no listado)" como el grupo de usuarios, debe de incluir en los comentarios que grupo necesita que sea generado',
+'At least one start/end combination must be entered when using "Repeating Monthly"': 'Por lo menos debe de existir una combinación de incio/fin cuando usa "Repetir mensualmente"',
+'At least one date/start/end combination must be entered when using "List of Dates/Times"': 'Por lo menos debe de existir una fecha de inicio/fin cuando usa "Fechas y horas específicas"',
+'Sunday': 'Domingo',
+'Monday': 'Lunes',
+'Tuesday': 'Martes',
+'Wednesday': 'Miércoles',
+'Thursday': 'Jueves',
+'Friday': 'Viernes',
+'Saturday': 'Sábado',
+'This must be a valid time that is greater than the start time': 'El horario seleccionado debe ser posterior a la hora de inicio'
+}
