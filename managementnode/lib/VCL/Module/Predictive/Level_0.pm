17f107aafb3e19658e91c7c95ad3b2cfb0646e33
Josh Thompson <jfthomps@apache.org>
2013-02-21 20:02:20 +0000

    updated $VERSION from 2.3.1 to 2.3.2

--- managementnode/lib/VCL/Module/Predictive/Level_0.pm
+++ managementnode/lib/VCL/Module/Predictive/Level_0.pm
@@ -43,7 +43,7 @@ use lib "$FindBin::Bin/../../..";
 use base qw(VCL::Module::Predictive);
 
 # Specify the version of this module
-our $VERSION = '2.3.1';
+our $VERSION = '2.3.2';
 
 # Specify the version of Perl to use
 use 5.008000;
--- managementnode/lib/VCL/utils.pm
+++ managementnode/lib/VCL/utils.pm
@@ -43,7 +43,7 @@ use lib "$FindBin::Bin/..";
 use base qw();
 
 # Specify the version of this module
-our $VERSION = '2.3.1';
+our $VERSION = '2.3.2';
 
 # Specify the version of Perl to use
 use 5.008000;
