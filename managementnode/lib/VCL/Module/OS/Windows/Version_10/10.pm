7d8db2898b64c8fbde42423e6871eeb8a1c6c4d7
Andrew Kurth <arkurth@apache.org>
2015-08-11 18:53:24 +0000

    VCL-893 Added Version_10 directory and 10.pm which currently inherits from 8.pm.

---
+++ managementnode/lib/VCL/Module/OS/Windows/Version_10/10.pm
@@ -0,0 +1,82 @@
+#!/usr/bin/perl -w
+###############################################################################
+# $Id: $
+###############################################################################
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+###############################################################################
+
+=head1 NAME
+
+VCL::Module::OS::Windows::Version_10::10.pm - VCL module to support deployment of Windows 10.x operating systems
+
+=head1 DESCRIPTION
+
+ This module provides support for the deployment of Windows 10.x operating systems.
+
+=cut
+
+##############################################################################
+package VCL::Module::OS::Windows::Version_10::10;
+
+# Specify the lib path using FindBin
+use FindBin;
+use lib "$FindBin::Bin/../../../../..";
+
+# Configure inheritance
+# Use the Windows 8 module for the time being - no changes are necessary
+use base qw(VCL::Module::OS::Windows::Version_6::8);
+
+# Specify the version of this module
+our $VERSION = '2.4.2';
+
+# Specify the version of Perl to use
+use 5.008000;
+
+use strict;
+use warnings;
+use diagnostics;
+
+use VCL::utils;
+
+##############################################################################
+
+=head1 CLASS VARIABLES
+
+=cut
+
+=head2 $SOURCE_CONFIGURATION_DIRECTORY
+
+ Data type   : Scalar
+ Description : Location on management node of script/utilty/configuration
+               files needed to configure the OS. This is normally the
+               directory under the 'tools' directory specific to this OS. For
+               Windows 10, the directory is:
+               tools/Windows_10
+
+=cut
+
+our $SOURCE_CONFIGURATION_DIRECTORY = "$TOOLS/Windows_10";
+
+##############################################################################
+
+1;
+__END__
+
+=head1 SEE ALSO
+
+L<http://cwiki.apache.org/VCL/>
+
+=cut
